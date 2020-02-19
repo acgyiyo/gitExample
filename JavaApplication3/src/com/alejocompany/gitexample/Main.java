c9b03f0677d5efba40a1bfd4642976cc031e9734
ALEJOZ-PC\ALEJANDRO <acgyiyo1@gmail.com>
2020-02-18 22:00:06 -0500

    agregado nueva linea

--- JavaApplication3/src/com/alejocompany/gitexample/Main.java
+++ JavaApplication3/src/com/alejocompany/gitexample/Main.java
@@ -4,6 +4,7 @@ package com.alejocompany.gitexample;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("");
         System.out.println("Hola Mundo");
     }
     
